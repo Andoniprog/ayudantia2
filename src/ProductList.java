bf6cf4e786732da7d5f4f5f339951a3f172b12c1
iyopz <chinomadit0777@gmail.com>
2023-10-11 17:48:27 -0300

    anhadi existByName, deleteById, get

--- src/ProductList.java
+++ src/ProductList.java
@@ -17,6 +17,27 @@ public class ProductList {
 
     }
 
+    public Product get(int index) {
+        return products[index];
+    }
 
+    public boolean deleteById(int idProducto) {
+        for (int i = 0; i < quantityProduct; i++) {
+            if (products[i].getId() == idProducto) {
+                products[i] = null;
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean existByName(String name) {
+        for(int i = 0; i < quantityProduct; i++) {
+            if (products[i].getName().equals(name)) {
+                return true;
+            }
+        }
+        return false;
+    }
 
 }
